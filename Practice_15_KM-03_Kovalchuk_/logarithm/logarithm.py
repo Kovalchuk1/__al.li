5547c9cff99e971a859bc778e6d297aa89e33382
Kovalchuk1 <72132862+Kovalchuk1@users.noreply.github.com>
2020-12-23 21:17:10 +0200

    Create logarithm.py

---
+++ Practice_15_KM-03_Kovalchuk_/logarithm/logarithm.py
@@ -0,0 +1,16 @@
+import math
+
+
+def log(a, b):
+    result = math.log(b, a)
+    return result
+
+
+def ln(b):
+    result = math.log(b)
+    return result
+
+
+def lg(b):
+    result = math.log10(b)
+    return result
